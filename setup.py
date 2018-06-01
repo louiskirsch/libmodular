189112c8ac82c6a2766eb2e7b0b056b5095d548b
Louis Kirsch <louis@louiskirsch.com>
2018-06-01 16:06:23 +0100

    Added setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='libmodular',
+    version='1.0',
+    packages=['libmodular'],
+    url='http://louiskirsch.com/libmodular',
+    license='',
+    author='Louis Kirsch',
+    author_email='mail@louiskirsch.com',
+    description=''
+)
